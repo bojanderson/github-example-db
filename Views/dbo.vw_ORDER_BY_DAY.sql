b16d9147e472add52be52eda5651c2e6cca09b3a
Stu Perlman <sgperlman@usesi.com>
2020-12-10 10:23:19 -0500

    Changed vw_ORDER_BY_DAY dropped sp_YoBo

---
+++ Views/dbo.vw_ORDER_BY_DAY.sql
@@ -0,0 +1,20 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+
+
+CREATE VIEW [dbo].[vw_ORDER_BY_DAY]
+AS
+
+	SELECT
+    	    ORD_DATE
+    	,	COUNT(*) as [ORD_COUNT]
+		,	'Static' as myChange
+    FROM
+    	dbo.ORDERS
+    GROUP BY ORD_DATE
+
+
+GO
